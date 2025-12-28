c8ede87a126b47d9f9e72458760046da76ced794
Rostyslav bc2025-4 <suhoverskijrostislav@gmail.com>
2025-12-29 00:30:02 +0200

    Setup UI: Added custom fonts, app icon and basic theme

---
+++ app/src/main/java/com/example/logos/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.logos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.magnifier
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.logos.ui.theme.LogosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LogosTheme {
+                        Scaffold(containerColor = colorResource(R.color.logos_main)) { innerPadding ->
+                            Greeting(
+                                name = "user",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                       }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LogosTheme {
+        Greeting("прікол")
+    }
+}
